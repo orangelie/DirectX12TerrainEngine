c0ebad0a3862c5167ded4ab16a5f5fe49e6d888e
orangelie <sw031489@naver.com>
2022-05-18 05:11:51 +0900

    TerrainEngine
    TerrainEngine

--- STATIC/HEADER/PUBLIC/Mesh/GeometryGenerator.h
+++ STATIC/HEADER/PUBLIC/Mesh/GeometryGenerator.h
@@ -68,9 +68,7 @@ namespace orangelie {
 			~GeometryGenerator();
 
 			static MeshData CreateGrid(float width, float depth, uint32 m, uint32 n);
-			static MeshData CreateBox(float width, float height, float depth, uint32 numSubdivisions);
-			static void Subdivide(MeshData& meshData);
-			static Vertex MidPoint(const Vertex& v0, const Vertex& v1);
+			static MeshData CreateSphere(float radius, uint32 sliceCount, uint32 stackCount);
 
 		private:
 
